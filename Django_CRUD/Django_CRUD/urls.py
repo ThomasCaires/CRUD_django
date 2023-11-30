1fd5a488944abd5dcda4a270585316f74833c9b1
Thomas caires <136385662+ThomasCaires@users.noreply.github.com>
2023-11-29 21:50:06 -0300

    class views
    implementando views baseadas em classes

--- Django_CRUD/Django_CRUD/urls.py
+++ Django_CRUD/Django_CRUD/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
-from app_CRUD import views
+from django.urls import path, include
+from app_CRUD.views import UserCreateView, UserListView, UserUpdateView, UserDeleteView
+from django.contrib import admin
 
 urlpatterns = [
     #route, view, reference
-    #pagina inicial
-    path('',views.home,name='home'),
-    path('users/',views.users,name='user_list')
+    path('',UserListView.as_view(),name='user_list'),
+    path('users',UserCreateView.as_view(),name='users_create'),
+    path('users/<int:pk>/',UserUpdateView.as_view(),name='user_update'),
+    path('users/<int:pk>/',UserDeleteView.as_view(),name='user_delete'),
+    #admin
+    path('admin/', admin.site.urls),  
 ]
--- Django_CRUD/app_CRUD/views.py
+++ Django_CRUD/app_CRUD/views.py
@@ -1,18 +1,35 @@
 from django.shortcuts import render
 from .models import User
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy as rl
 # Create your views here.
 def home(request):
     return render(request, 'users/home.html')
 
-def users(request):
-    #salvar os dadosda tela no Database
-    new_user = User()
-    new_user.name = request.POST.get("name")
-    new_user.age = request.POST.get("age")
-    new_user.save()
-    #exibir os users cadastrados na pagina
-    users = {
-        'users' : User.objects.all()
-    }
-    #retornar dados para a pagina de listagem
-    return render(request, 'users/users.html', users)
+#listar usuario
+
+class UserListView(ListView):
+    model = User
+    template_name = 'users/user_list.html'
+    context_object_name = 'user_list'
+
+#criar usuarios
+class UserCreateView(CreateView):
+    model = User
+    fields = ['name', 'age']
+    template_name = 'users/user_form.html'
+    context_object_name = 'user_form'
+    success_url = rl('user_list')
+    
+#editar usuario
+class UserUpdateView(UpdateView):
+    model = User
+    fields = ['name', 'age']
+    template_name = 'users/user_form.html'
+    content_type = 'user_update'
+    success_url = rl('user_list')
+
+#deletar usuario
+class UserDeleteView(DeleteView):
+    model = User
+    template_name = 'users/user_confirm_delete.html'
